a4d0f0731cb35828027f782b475853c98daa34d7
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2026-05-22 11:47:43 +0300

    feat: configure app routes with auxiliary side-panel outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { ArticleListComponent } from './features/article-list/article-list.component';
+import { ArticleCreateComponent } from './features/article-create/article-create.component';
+import { ArticleDetailComponent } from './features/article-detail/article-detail.component';
+import { CommentsPanelComponent } from './features/comments/comments-panel.component';
+import { unsavedChangesGuard } from './core/guards/unsaved-changes.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ArticleListComponent,
+  },
+  {
+    path: 'create',
+    component: ArticleCreateComponent,
+    canDeactivate: [unsavedChangesGuard],
+  },
+  {
+    path: 'article/:id',
+    component: ArticleDetailComponent,
+    children: [
+      {
+        path: 'comments',
+        component: CommentsPanelComponent,
+        outlet: 'side-panel',
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
