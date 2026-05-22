bf22699c78b1bcf40647901f43a302b4e947d753
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2026-05-22 12:17:15 +0300

    feat: add NavbarComponent

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavbarComponent {
+  constructor(private router: Router) { }
+
+  goHome(): void {
+    this.router.navigate(['/']);
+  }
+
+  goToCreate(): void {
+    this.router.navigate(['/create']);
+  }
+}
